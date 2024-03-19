d58e930bac92f9461e9d6ec3d9949c0cc8eaeb0a
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-19 14:31:50 +0300

    Configuration is created and add annotation

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -0,0 +1,10 @@
+package com.cydeo.streotype_annotation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cydeo.streotype_annotation") // where it is looking that @Component annotation, check all the classes
+public class PcConfig {
+
+}
