eb5050c5220ec8692210821125de3e71ef5228f3
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-19 15:52:09 +0300

    Bean Annotation is done

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.cydeo.streotype_annotation") // where it is looking that @Component annotation, check all the classes
+@ComponentScan("com.cydeo.streotype_annotation")
+// we  write base package ==> where it is looking that @Component annotation, check all the classes
+
 public class PcConfig {
 
 }
