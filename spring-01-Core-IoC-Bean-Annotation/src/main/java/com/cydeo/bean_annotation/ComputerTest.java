7fe398ef73aaf4fbce2223b2b54261b5a715c553
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-30 12:36:59 +0300

    Bean Annotation review

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -14,9 +14,10 @@ public class ComputerTest {
     public static void main(String[] args) {
 
         System.out.println("Creating Container");
-
+        //Container is the place, the box the pull container that spring is creating object and putting inside the container.
         //Creating container by using Application Context
         ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, RandomConfig.class);
+        // hey Spring, go this Config class, if we use @Bean annotation, and the look at the Bean over there
         // whatever configuration you want to edit, you can just say comma and put other one and comma put other one.
 
         //Creating container by using BeanFactory
@@ -25,9 +26,11 @@ public class ComputerTest {
         System.out.println("************Retrieving the beans******************");
 
         SonyMonitor sony = container.getBean(SonyMonitor.class);
+        // you define the bean type, if you only have on Bean matching with your Spring, will not get confused and bring it your Bean
         DellCase dell = container.getBean(DellCase.class);
         AsusMotherboard asus = container.getBean(AsusMotherboard.class);
 
+        // why we are getting those, beacuse we're gonna providing this PC object
         PC myPc = new PC(dell,sony,asus);
 
         myPc.powerUp();
--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -10,24 +10,36 @@ import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+// Configuration is the way or the ways to tell Spring which classes I want Spring manage
+// if the object manage by Spring, it is called Bean,
+// Bean is the objects that is manage by the Spring that we have the container
+// if you want to tell hey Spring this are the objects that I want you to manage (these are the beans)
+// we need to create a method, that is returning the objects that you want Spring to add containers
+//and then that method with the @Bean
 
-@Configuration
+@Configuration  // this annotation is used to lable the class to make that class as a Configuration class where we gonna defines our beans
 public class ComputerConfig {
 
-    @Bean(name="sony")
+    @Bean(name="sony")  // this annotation is used to label the methods that are returning objects
+    // so we are in the implementatipn we are responsible for using new keyword one time create object on time,
+    // the rest will handled by Spring
     public Monitor monitorSony(){
         return new SonyMonitor("25 inch Beast","Sony",25);
     }
 
-    @Bean(name="sony2")
+    @Bean(name="sony2") // giving name to your Bean, custom name
     public Monitor monitorSony2(){
 
         return new SonyMonitor("40 inch Beast","Sony",40);
     }
 
+    /*
+    if you would like to use static always same bean we can use Primary,
+    if you would like to adopt the bean in different situation we can use bean Name
+     */
 
     @Bean
-    @Primary
+    @Primary // to make default
     public Monitor monitorAcer(){
         return new AcerMonitor("23 inch Beast","Acer",23);
     }
