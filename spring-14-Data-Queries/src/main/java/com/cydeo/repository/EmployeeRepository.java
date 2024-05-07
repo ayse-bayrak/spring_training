9bd512191170d5fbd17814d94924ff9af6c1450a
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-05-07 23:38:59 +0300

    Drived Queries

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
@@ -28,4 +29,14 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //Display all employees with salaries less than
     List<Employee> findBySalaryLessThan(Integer salary);
+
+    //Display all employees that has been hired between "" and ""
+    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
+
+    //Display all employees where salaries greate and equal to "" order
+    List<Employee> findBySalaryGreaterThanEqualOrderBySalary(Integer salary);
+
+    //Display top unique 3 employees that is making less than ""
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
 }
