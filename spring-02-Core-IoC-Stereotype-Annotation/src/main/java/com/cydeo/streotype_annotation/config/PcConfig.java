2d4e4dae3dea720cbcb056218020df88c1851e6a
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-07 21:22:48 +0300

    Stereotype review and some changes

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 // no bean no method
 // i have something else..@ComponentScan
-// hey spring i want to search all packages, if the class @Component, create the Bean and put inside the container
+// hey spring i want to search all packages, if the class @Component, create the Bean
+// and put inside the container
 @Configuration
 @ComponentScan("com.cydeo.streotype_annotation")
 // we  write base package ==> where it is looking that @Component annotation, check all the classes
