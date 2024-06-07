4892f8e54609d97d643a9feeeeb0650d08001852
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-07 21:23:02 +0300

    Core-Abstraction review and some changes

--- spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/model/Comment.java
@@ -12,7 +12,7 @@ import lombok.Data;
 //we do not put @Component, because this is not class that spring create
 //Under the model package, the classes that we have we gonna use for get the information
 //or provide the information
-//Model/Entity Object never get @Component, because no need inject this one, i don't need to Spring cretae object
+//Model/Entity Object never get @Component, because no need inject this one, i don't need to Spring create object
 //because this information will be field by user
 //shortly, if you use class to get the information that class will not go to container
 @Data
