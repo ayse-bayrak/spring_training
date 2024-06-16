3d7e80f90ce10c87c7d0c060d89dec39cf12e7d9
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-17 00:04:13 +0300

    CourseController_ResponseWrapper v3 is created

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -0,0 +1,42 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/courses/api/v3")
+public class CourseController_ResponseWrapper {
+// this version is final version to create API endpoints, because we need to modify output (vhange status add header etc) and
+// we need to customize Json object also, so we use ResponseEntity (ready class which have a lot of method like status, headers, body, ok ...)
+// and ResponseWrapper(custom class) class
+    private final CourseService courseService;
+
+    public CourseController_ResponseWrapper(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getAllCourses() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("Verion", "Cydeo")
+                .body(new ResponseWrapper("courses succesfully retrieved" , courseService.getCourses()));
+        //              new ResponseWrapper(     String message         , Object data) // at the end of courseService.getCourses()) is Object data (List<CourseDTO>)
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") long courseId) {
+        return ResponseEntity
+                .ok(new ResponseWrapper("course "+courseId+" retrieved", courseService.getCourseById(courseId)));
+    }
+    }
