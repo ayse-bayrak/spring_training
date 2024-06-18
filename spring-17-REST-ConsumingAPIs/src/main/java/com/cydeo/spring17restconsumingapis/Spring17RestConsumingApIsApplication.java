e70f3f8e37b9a63fad4277f7b628bf8ccc0e9a5e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-19 00:48:24 +0300

    @EnableFeignClients is added

--- spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/spring17restconsumingapis/Spring17RestConsumingApIsApplication.java
+++ spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/spring17restconsumingapis/Spring17RestConsumingApIsApplication.java
@@ -2,10 +2,12 @@ package com.cydeo.spring17restconsumingapis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Spring17RestConsumingApIsApplication {
 
 	public static void main(String[] args) {
@@ -16,5 +18,9 @@ public class Spring17RestConsumingApIsApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+	// I need to certain methods different classes, i need to inject this one,
+	// but i need to cretae firt @Bean and I can not @Component bevause class is not mine
+	// if the class is not yours but if you need bean from this,
+	// we can just create it in the Runner class
 
 }
