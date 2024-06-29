89b971f5db5e48c4b45a78fdbc6eaa546672f1fc
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-29 15:18:48 +0300

    Create new 2 module for Keycloak testing

---
+++ spring-20-REST-Security-Keycloak-LicenseApp/src/test/java/com/cydeo/spring20restsecuritykeycloaklicenseapp/Spring20RestSecurityKeycloakLicenseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring20restsecuritykeycloaklicenseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring20RestSecurityKeycloakLicenseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
